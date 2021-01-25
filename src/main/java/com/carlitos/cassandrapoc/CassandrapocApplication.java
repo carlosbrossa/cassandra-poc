61a105117166b199471beb263aface2eab01ce68
carlitos <carlos.brossa@dasa.com.br>
2021-01-24 22:15:41 -0300

    Initial commit

---
+++ src/main/java/com/carlitos/cassandrapoc/CassandrapocApplication.java
@@ -0,0 +1,13 @@
+package com.carlitos.cassandrapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandrapocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandrapocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlitos/cassandrapoc/CassandrapocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlitos.cassandrapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandrapocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
